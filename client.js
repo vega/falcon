1410d79f6619beb277d9980a5746742f1535e258
Dominik Moritz <domoritz@gmail.com>
2016-10-04 16:30:39 -0700

    Send some data

--- client.js
+++ client.js
@@ -1,6 +1,10 @@
+const host = window.document.location.host.replace(/:.*/, '');
+const ws = new WebSocket('ws://' + host + ':4080');
 
-var host = window.document.location.host.replace(/:.*/, '');
-var ws = new WebSocket('ws://' + host + ':4080');
-ws.onmessage = function (event) {
+ws.onmessage = event => {
   console.log(event);
+};
+
+ws.onopen = event => {
+  ws.send('Hello world'); 
 };
