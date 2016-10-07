9f424d2241a3dbf2ae71679ae7ae5826d4ee1572
Matthew Conlen <mc@mathisonian.com>
2016-10-07 11:15:29 -0700

    add id field to the interfaces. update tsconfig to make atom stop complaining

--- interfaces.d.ts
+++ interfaces.d.ts
@@ -1,20 +1,24 @@
 type Rng = [number, number];
 
 declare type Request = {
+    id: number,
     type: 'query',
-    dims: {[domain: string] : {
+    dims: {[domain: string]: {
         range: Rng,
         numBins: number
     }}
 } | {
+    id: number,
     type: 'range',
     dims: string[]
 };
 
 declare type Result = {
+    id: number,
     type: 'query',
     data: {[dim: string]: number[]}
 } | {
     type: 'range',
+    id: number,
     ranges: {[dim: string]: Rng}
 }
