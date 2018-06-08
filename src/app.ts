02ea2891d3e3c36473b87b18626a4bd0d86e4867
Dominik Moritz <domoritz@gmail.com>
2018-06-08 15:52:25 -0700

    make an app class

--- src/app.ts
+++ src/app.ts
@@ -1,79 +1,53 @@
-import { select, range, histogram } from "d3";
+import { select, range, histogram, Selection, BaseType } from "d3";
 import { createView } from "./view";
 import { stepSize } from "./util";
 
-const VIEWS: View[] = [
-  {
-    bins: 25,
-    dimension: "ARR_DELAY",
-    name: "ARR_DELAY",
-    range: [-10, 100],
-    title: "Arrival Delay",
-    type: "1D"
-  },
-  {
-    bins: 25,
-    dimension: "DISTANCE",
-    name: "DISTANCE",
-    range: [50, 2000],
-    title: "Distance",
-    type: "1D"
-  },
-  {
-    bins: 25,
-    dimension: "DEP_DELAY",
-    name: "DEP_DELAY",
-    range: [-10, 100],
-    title: "Departure Delay",
-    type: "1D"
+export class App {
+  public constructor(
+    private el: Selection<BaseType, {}, HTMLElement, any>,
+    private views: View[],
+    private data: { [name: string]: any[] }
+  ) {
+    console.log(data);
+    this.initialize();
   }
-];
 
-export function app(data: { [name: string]: any[] }) {
-  const app = select("#app");
-
-  // add for now to clear the view
-  (app.node() as any).innerHTML = "";
-
-  console.log(data);
-
-  app
-    .selectAll(".view")
-    .data(VIEWS)
-    .enter()
-    .append("div")
-    .attr("class", "view")
-    .each(function(view: View) {
-      const step = stepSize(view.range, view.bins);
-      const bins = range(view.range[0], view.range[1] + step, step);
-
-      const vegaView = createView(
-        select(this).node() as Element,
-        view.title,
-        step,
-        bins
-      );
-
-      vegaView.addSignalListener(
-        "pixelRange",
-        (name: string, value: [number, number]) => {
-          console.log(name, value);
-        }
-      );
-
-      const hist = histogram()
-        .domain(view.range)
-        .thresholds(bins)(data[view.name])
-        .map(d => d.length);
-
-      vegaView
-        .insert(
-          "table",
-          hist.map((d, i) => ({
-            value: bins[i],
-            count: d
-          }))
-        )
-        .run();
-    });
+  private initialize() {
+    const data = this.data;
+    this.el
+      .selectAll(".view")
+      .data(this.views)
+      .enter()
+      .append("div")
+      .attr("class", "view")
+      .each(function(view: View) {
+        const step = stepSize(view.range, view.bins);
+        const bins = range(view.range[0], view.range[1] + step, step);
+        const vegaView = createView(
+          select(this).node() as Element,
+          view.title,
+          step,
+          bins
+        );
+        vegaView.addSignalListener(
+          "pixelRange",
+          (name: string, value: [number, number]) => {
+            console.log(name, value);
+          }
+        );
+        const hist = histogram()
+          .domain(view.range)
+          .thresholds(bins)(data[view.name])
+          .map(d => d.length);
+        vegaView
+          .insert(
+            "table",
+            hist.map((d, i) => ({
+              value: bins[i],
+              count: d
+            }))
+          )
+          .run();
+      });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { app } from "./app";
+import { App } from "./app";
+import { select } from "d3";
 
 const data = require("../data/flights-10k.json");
 
@@ -9,4 +10,36 @@ data.DISTANCE = new Int16Array(data.DISTANCE);
 data.ARRIVAL = data.ARRIVAL.map(d => new Date(d * 1000));
 data.DEPARTURE = data.DEPARTURE.map(d => new Date(d * 1000));
 
-app(data);
+const VIEWS: View[] = [
+  {
+    bins: 25,
+    dimension: "ARR_DELAY",
+    name: "ARR_DELAY",
+    range: [-10, 100],
+    title: "Arrival Delay",
+    type: "1D"
+  },
+  {
+    bins: 25,
+    dimension: "DISTANCE",
+    name: "DISTANCE",
+    range: [50, 2000],
+    title: "Distance",
+    type: "1D"
+  },
+  {
+    bins: 25,
+    dimension: "DEP_DELAY",
+    name: "DEP_DELAY",
+    range: [-10, 100],
+    title: "Departure Delay",
+    type: "1D"
+  }
+];
+
+const el = select("#app");
+
+// add for now to clear the view
+(el.node() as any).innerHTML = "";
+
+new App(el, VIEWS, data);
