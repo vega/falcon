0cf6f507a8360111435b7d088149bc50b3acafba
Dominik Moritz <domoritz@gmail.com>
2018-06-28 09:23:16 -0700

    Refactor logger code

--- src/logger.ts
+++ src/logger.ts
@@ -20,11 +20,11 @@ interface MouseRecord {
 
 export class Logger<V extends string> {
   private static maxtries = 3; // maximum # of tries to send things to server
-  private logContainer: Array<Record>;
-  private stagingContainer: Array<Record>;
+  private logContainer: Record[] = [];
+  private stagingContainer: Record[] = [];
 
-  private mouseLogContainer: Array<MouseRecord>;
-  private stagingMouseContainer: Array<MouseRecord>;
+  private mouseLogContainer: MouseRecord[] = [];
+  private stagingMouseContainer: MouseRecord[] = [];
 
   /**
    * Constructs the logger
@@ -34,20 +34,12 @@ export class Logger<V extends string> {
     private taskid?: string,
     private logUrl?: string
   ) {
-    this.logContainer = [];
-    this.stagingContainer = [];
-    this.mouseLogContainer = [];
-    this.stagingMouseContainer = [];
-    this.userid = userid;
-    this.taskid = taskid;
-    this.logUrl = logUrl;
-
     document.onmousemove = throttle(this.trackMouse(), 50);
     // TODO: start a periodic ten second process to ship stuff
-    setInterval(this.writeToLog(), 10000);
+    setInterval(this.writeToLog.bind(this), 10000);
   }
 
-  /* 
+  /*
   * track global mouse position
   */
   private trackMouse() {
@@ -91,52 +83,54 @@ export class Logger<V extends string> {
   }
 
   public writeToLog() {
-    const self = this;
-    return function() {
-      //move from log container to staging container
-      self.stagingContainer.push(
-        ...self.logContainer.splice(0, self.logContainer.length)
-      );
+    //move from log container to staging container
+    this.stagingContainer.push(
+      ...this.logContainer.splice(0, this.logContainer.length)
+    );
 
-      self.stagingMouseContainer.push(
-        ...self.mouseLogContainer.splice(0, self.mouseLogContainer.length)
-      );
+    this.stagingMouseContainer.push(
+      ...this.mouseLogContainer.splice(0, this.mouseLogContainer.length)
+    );
 
-      if (
-        self.stagingContainer.length === 0 &&
-        self.stagingMouseContainer.length === 0
-      ) {
-        return;
-      }
-      console.log("writing to server...");
-      // send contents to server
-      let tries = 0;
-      const doFetch = function() {
-        fetch(self.logUrl, {
-          body: JSON.stringify({
-            userid: self.userid,
-            taskid: self.taskid,
-            log: self.stagingContainer,
-            mouseLog: self.stagingMouseContainer
-          }),
-          headers: { "Content-Type": "application/json" },
-          method: "POST"
-        }).then(response => {
-          if (response.status !== 200) {
-            // FAIL
+    if (
+      this.stagingContainer.length === 0 &&
+      this.stagingMouseContainer.length === 0
+    ) {
+      return;
+    }
+    console.log("writing to server...");
+    // send contents to server
+    let tries = 0;
+    const doFetch = () => {
+      fetch(this.logUrl, {
+        body: JSON.stringify({
+          userid: this.userid,
+          taskid: this.taskid,
+          log: this.stagingContainer,
+          mouseLog: this.stagingMouseContainer
+        }),
+        headers: { "Content-Type": "application/json" },
+        method: "POST"
+      })
+        .then(response => {
+          if (response.ok) {
+            this.stagingContainer = [];
+          } else {
             tries++;
             if (tries < Logger.maxtries) {
+              console.log(
+                `Writing failed. Trying again (${tries}/${Logger.maxtries}).`
+              );
               doFetch();
             } else {
-              throw response.status;
+              throw new Error(response.statusText);
             }
-          } else {
-            // PASS
-            self.stagingContainer = [];
           }
+        })
+        .catch(error => {
+          throw error;
         });
-      };
-      doFetch();
     };
+    doFetch();
   }
 }
