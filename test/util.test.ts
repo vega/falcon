8b7dd4ca463fb0c946675f285534f32040ce6c46
Dominik Moritz <domoritz@gmail.com>
2019-02-23 18:08:58 -0800

    Fix tests

--- test/util.test.ts
+++ test/util.test.ts
@@ -1,35 +1,5 @@
 import { BitSet, union } from "./../src/bitset";
-import ndarray from "ndarray";
-import { _chEmd, chEmd, omit, repeatInvisible } from "./../src/util";
-
-describe("emb", () => {
-  const a = [1, 2, 4, 6, 7, 8];
-  const b = [0, 3, 3, 4, 5, 7];
-
-  // https://colab.research.google.com/drive/1wLWvtd0rfKcqZJK2kch9CGww3p5QrZhG
-  const emd = 0.7142857142857143;
-
-  test("cumulative histogram emd is correct", () => {
-    expect(_chEmd(ndarray(a), ndarray(b), 8, 7)).toBe(emd);
-  });
-
-  test("exposed emd produces the same result", () => {
-    expect(chEmd(ndarray(a), ndarray(b))).toBe(emd / 6);
-  });
-
-  test("emd works for views", () => {
-    expect(
-      chEmd(ndarray([1, 2, 4, 6, 7, 8], [6]), ndarray([0, 3, 3, 4, 5, 7], [6]))
-    ).toBe(emd / 6);
-
-    expect(
-      chEmd(
-        ndarray([1, 2, 4, 6, 7, 8, 1, 2], [6]),
-        ndarray([0, 3, 3, 4, 5, 7, 8, 9], [6])
-      )
-    ).toBe(emd / 6);
-  });
-});
+import { omit, repeatInvisible } from "./../src/util";
 
 describe("union", () => {
   test("unions two bitsets", () => {
