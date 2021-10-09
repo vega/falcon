908ce2d914575b29af4efcb8d407233777f1c10b
Dominik Moritz <domoritz@gmail.com>
2021-10-08 18:21:10 -0700

    chore: fix typo

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const HIST_TYPE = Int32Array;
-export const CUM_ARR_TYPE = Float32Array; // use float because we arer storing sums
+export const CUM_ARR_TYPE = Float32Array; // use float because we are storing sums
