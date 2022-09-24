876897d2f05552bacac21053c402b09721abfad5
xnought <bertuccd@oregonstate.edu>
2022-09-24 10:45:18 -0700

    feat: create svelte example that imports falcon

---
+++ examples/flights-in-browser/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { falconName } from "../../../src/falcon";
+</script>
+
+<main>
+	<h1>Flights in Browser</h1>
+	<p>Interactive visualization of flights with {falconName}</p>
+</main>
+
+<style>
+</style>
---
+++ src/falcon.ts
@@ -0,0 +1,3 @@
+// just to test if I can import this file from examples/flights-in-browser
+// indeed I can!
+export const falconName = "falcon-vis";
