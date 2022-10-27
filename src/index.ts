757cb6bdf4a223ff6057621cc10923064e66acf9
xnought <bertuccd@oregonstate.edu>
2022-10-26 19:11:51 -0700

    feat: move old code as new src and keep data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const nice = "one";
+import pkg from "../package.json";
+
+export * from "./api";
+export * from "./app";
+export * from "./basic";
+export * from "./config";
+export * from "./db";
+export * from "./loggers";
+export * from "./util";
+
+export const version = pkg.version;
