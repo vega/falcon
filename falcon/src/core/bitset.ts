312ac88ebbe1852b6d1482785168d0fc9a7eb778
xnought <bertuccd@oregonstate.edu>
2023-01-15 21:49:16 -0800

    feat: move bitset to core and add tests

---
+++ falcon/src/core/bitset.ts
@@ -0,0 +1,61 @@
+// from https://github.com/mikolalysenko/minimal-bit-array/blob/master/bitarray.js with modifications
+
+export class BitSet {
+  private _bits: Uint32Array;
+
+  constructor(public readonly length: number, bits?: Uint32Array) {
+    this._bits = bits || new Uint32Array((length >>> 5) + 1);
+  }
+
+  public get(i: number) {
+    i |= 0;
+    return !!(this._bits[i >>> 5] & (1 << (i & 31)));
+  }
+
+  public get bits() {
+    return this._bits;
+  }
+
+  public set(i: number, v: boolean) {
+    i |= 0;
+    const idx = i >>> 5;
+    const bit = 1 << (i & 31);
+    if (v) {
+      this._bits[idx] |= bit;
+    } else {
+      this._bits[idx] &= ~bit;
+    }
+
+    return this;
+  }
+
+  public union(other: BitSet) {
+    for (let i = 0; i < this._bits.length; i++) {
+      this._bits[i] = this._bits[i] | other._bits[i];
+    }
+  }
+}
+
+/**
+ * Compute the union of all bit sets. The sets need to have the same length.
+ * If you only pass in a single bit set, this function will not make a copy.
+ */
+export function union(...sets: BitSet[]): BitSet | null {
+  if (sets.length == 0) {
+    return null;
+  }
+
+  if (sets.length == 1) {
+    return sets[0];
+  }
+
+  // copy of first set
+  const out = new BitSet(sets[0].length, new Uint32Array(sets[0].bits));
+
+  for (let i = 1; i < sets.length; i++) {
+    const set = sets[i];
+    out.union(set);
+  }
+
+  return out;
+}
--- falcon/tests/bitset.test.ts
+++ falcon/tests/bitset.test.ts
@@ -1,8 +1,34 @@
-import { BitSet, union } from "../src/old/bitset";
-import { omit, repeatInvisible } from "../src/old/util";
+import { BitSet, union } from "../src/core/bitset";
 
-describe("union", () => {
-  test("unions two bitsets", () => {
+describe("Bitset creation", () => {
+  it("import defined", () => {
+    expect(BitSet).toBeDefined();
+  });
+
+  it("construction with one integer", () => {
+    const length = 1;
+    const set = new BitSet(length);
+
+    expect(set).toBeDefined();
+    expect(set.length).toBe(length);
+    expect(set.bits[0]).toBe(0);
+  });
+
+  it("construction spanning multiple integers", () => {
+    const length = 33;
+    const set = new BitSet(length);
+
+    expect(set).toBeDefined();
+    expect(set.length).toBe(length);
+    expect(set.bits[0]).toBeDefined();
+    expect(set.bits[1]).toBeDefined();
+    expect(set.bits[0]).toBe(0);
+    expect(set.bits[1]).toBe(0);
+  });
+});
+
+describe("Bitset union", () => {
+  it("unions two bitsets", () => {
     const set1 = new BitSet(5);
     set1.set(0, true).set(1, true).set(2, true);
     const set2 = new BitSet(5);
@@ -26,26 +52,5 @@ describe("union", () => {
     expect(set2.get(2)).toBeTruthy();
     expect(set2.get(3)).toBeFalsy();
     expect(set2.get(4)).toBeTruthy();
-  });
-});
-
-describe("omit", () => {
-  test("omit does not change original map", () => {
-    const original = new Map<number, number>();
-    original.set(1, 1);
-    original.set(2, 2);
-    original.set(3, 3);
-    original.set(4, 4);
-    const copy = omit(original, 2, 3);
-    expect([...original.keys()]).toEqual([1, 2, 3, 4]);
-    expect([...copy.keys()]).toEqual([1, 4]);
-  });
-});
-
-describe("repeatInvisible", () => {
-  test("simple case", () => {
-    expect(repeatInvisible([1, 2, 3, 4, 5, 6, 7, 8, 9], 3.5, 8.5)).toEqual([
-      4, 4, 4, 4, 5, 6, 7, 8, 8,
-    ]);
   });
 });
