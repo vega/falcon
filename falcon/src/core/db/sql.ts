b725d853e7022e5849f9ce649dd970f3dcdfb76c
xnought <bertuccd@oregonstate.edu>
2023-01-21 14:58:15 -0800

    feat: sql skeleton

---
+++ falcon/src/core/db/sql.ts
@@ -0,0 +1,47 @@
+import { RangeDimension } from "../dimension";
+import { Interval } from "../util";
+import { View1D } from "../views";
+import {
+  AsyncOrSync,
+  BinnedCounts,
+  FalconDB,
+  Filters,
+  FalconIndex,
+} from "./db";
+import type { View } from "../views";
+
+export type SQLNameMap = Map<string, string>;
+
+export abstract class SQLDB implements FalconDB {
+  table: string;
+  nameMap: SQLNameMap;
+  constructor(table: string, nameMap: SQLNameMap) {
+    this.table = table;
+    this.nameMap = nameMap;
+  }
+
+  protected abstract query(q: string): Promise<Iterable<Record<string, any>>>;
+
+  extent(dimension: RangeDimension): AsyncOrSync<Interval<number>> {
+    return {} as AsyncOrSync<Interval<number>>;
+  }
+
+  length(): AsyncOrSync<number> {
+    return 0 as AsyncOrSync<number>;
+  }
+
+  histogramView1D(
+    view: View1D,
+    filters?: Filters | undefined
+  ): AsyncOrSync<BinnedCounts> {
+    return {} as AsyncOrSync<BinnedCounts>;
+  }
+
+  falconIndexView1D(
+    activeView: View1D,
+    passiveViews: View[],
+    filters: Filters
+  ): FalconIndex {
+    return {} as FalconIndex;
+  }
+}
