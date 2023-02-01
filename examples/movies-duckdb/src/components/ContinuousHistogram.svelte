9b4f0c43b2c8f2391cb212d1ab4ec948f7152e1a
xnought <bertuccd@oregonstate.edu>
2023-02-01 15:13:53 -0800

    feat: add duckdb movies

---
+++ examples/movies-duckdb/src/components/ContinuousHistogram.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import VLContinuous from "./VLContinuous.svelte";
+	import type { View1DState } from "falcon2";
+	export let state: View1DState;
+
+	export let title = "";
+	export let width = 400;
+	export let height = 125;
+	export let countLabel = "Count";
+	export let dimLabel = "";
+
+	$: data = convertFormat(state);
+
+	function convertFormat(state: View1DState) {
+		let newBins = [];
+		if (state) {
+			for (let i = 0; i < state.bin.length; i++) {
+				newBins.push({
+					bin: [state.bin[i].binStart, state.bin[i].binEnd],
+					count: state.total[i],
+					filteredCount: state.filter[i],
+				});
+			}
+		}
+		return newBins;
+	}
+</script>
+
+<VLContinuous
+	bins={data}
+	on:select
+	on:mouseenter
+	on:mouseleave
+	{title}
+	{width}
+	{height}
+	{countLabel}
+	{dimLabel}
+/>
